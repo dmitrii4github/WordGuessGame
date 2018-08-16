ef390e8e72d23eb3cfe70347218fef88099ba1d4
Dmitrii Zakharov <dzakharov@splunk.com>
2018-08-16 16:44:26 -0700

    Implemented letter replacement

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,5 @@
 document.onkeyup = function(event) {
 
-    // Determines which key was pressed.
-    var userGuess = event.key;
-    alert(userGuess);
-
     var musician = "Modern Talking";
 
     //Display the same number of '_' as the length of musician string
@@ -15,5 +11,25 @@ document.onkeyup = function(event) {
     var currentWord = document.getElementById("current_word"); 
     currentWord.textContent = musicianPlaceholder;
 
+    // Determines which key was pressed.
+    var userGuess = event.key;
+    alert(userGuess);
+
+    //Display the letter
+    userGuessIndex = musician.indexOf(userGuess[0]);
+    alert(userGuessIndex);
+    if (userGuessIndex != -1) {
+        musicianPlaceholder = "";
+        for (var i=0; i<musician.length; i++) {
+            if (i == userGuessIndex) {
+                musicianPlaceholder += musician[i];
+            } else {
+               musicianPlaceholder += "_";
+            }
+            musicianPlaceholder += " ";
+        }
+    }    
+    alert(musicianPlaceholder);
+    currentWord.textContent = musicianPlaceholder;
     
 }
