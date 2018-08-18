9c1db1b035811944dd393b0f29c363478f2b8abb
Dmitrii Zakharov <dzakharov@splunk.com>
2018-08-17 17:25:28 -0700

    Fixed bugs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,11 @@
 var musician = "M a d";
-var numberOfGuesses = 10;
+var numberOfGuesses = 11;
+var numberOfWins = 0;
 var lettersGuessed = "";
 var currentWord = null;
 var initializePlaceholder = true;
 var musicianPlaceholder = "";
+var guesses = [];
 
 document.onkeyup = function(event) {
 
@@ -24,46 +26,59 @@ document.onkeyup = function(event) {
 
     // Determines which key was pressed.
     var userGuess = event.key;
-    alert(userGuess);
+    // alert(userGuess);
 
-    //Display the letter
-    var userGuessIndices = getAllIndices(musician, userGuess[0]);
-    alert(userGuessIndices[0]);
-    alert(userGuessIndices[1]);
-    if (userGuessIndices.length != 0) {
+    if (guesses.indexOf(userGuess) == -1) {
 
-        //First, display the musician placeholder with letters that were just guessed
-        var newMusicianPlaceholder = "";
-        alert("starting loop");
-        alert("'"+musicianPlaceholder+"'");
-        alert("'"+newMusicianPlaceholder+"'");
-        for (var i=0; i<musicianPlaceholder.length; i++) {
-            if ((userGuessIndices.indexOf(i) != -1) || ((musicianPlaceholder[i] != "_") && (musicianPlaceholder[i] != " "))) {
-                newMusicianPlaceholder += musician[i];
-            } else if (musicianPlaceholder[i] == "_") {
-                newMusicianPlaceholder += "_";
-            } else {
-                newMusicianPlaceholder += " ";
+        guesses.push(userGuess);
+
+        //Display the letter
+        var userGuessIndices = getAllIndices(musician, userGuess[0]);
+        // alert(userGuessIndices[0]);
+        // alert(userGuessIndices[1]);
+        if (userGuessIndices.length != 0) {
+
+            //First, display the musician placeholder with letters that were just guessed
+            var newMusicianPlaceholder = "";
+            // alert("starting loop");
+            // alert("'"+musicianPlaceholder+"'");
+            // alert("'"+newMusicianPlaceholder+"'");
+            for (var i=0; i<musicianPlaceholder.length; i++) {
+                if ((userGuessIndices.indexOf(i) != -1) || ((musicianPlaceholder[i] != "_") && (musicianPlaceholder[i] != " "))) {
+                    newMusicianPlaceholder += musician[i];
+                } else if (musicianPlaceholder[i] == "_") {
+                    newMusicianPlaceholder += "_";
+                } else {
+                    newMusicianPlaceholder += " ";
+                }
+                // alert(newMusicianPlaceholder);
             }
-            alert(newMusicianPlaceholder);
+
+            musicianPlaceholder = newMusicianPlaceholder;
+
+            //Second, display what letters have been guessed
+            lettersGuessed += musician[userGuessIndices[0]];
+            lettersGuessed += " ";
+        }    
+        // alert(musicianPlaceholder);
+        currentWord.textContent = musicianPlaceholder;
+
+        numberOfGuesses--;
+
+        var guessNumber = document.getElementById("guess_number"); 
+        guessNumber.textContent = numberOfGuesses;
+
+        var lettersGuessedDiv = document.getElementById("letters_guessed");
+        lettersGuessedDiv.textContent = lettersGuessed;
+
+        if (musicianPlaceholder == musician) {
+            alert("You won!");
+            var winsString = document.getElementById("Wins");
+            numberOfWins++;
+            winsString.textContent = numberOfWins;
         }
 
-        musicianPlaceholder = newMusicianPlaceholder;
-
-         //Second, display what letters have been guessed
-         lettersGuessed += musician[userGuessIndices[0]];
-         lettersGuessed += " ";
-    }    
-    alert(musicianPlaceholder);
-    currentWord.textContent = musicianPlaceholder;
-
-    numberOfGuesses--;
-
-    var guessNumber = document.getElementById("guess_number"); 
-    guessNumber.textContent = numberOfGuesses;
-
-    var lettersGuessedDiv = document.getElementById("letters_guessed");
-    lettersGuessedDiv.textContent = lettersGuessed;
+    }
 
 }
 
